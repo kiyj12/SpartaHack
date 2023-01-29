51a79abae0df1e0906843569c57c552699044b27
kiyj12 <kiyj12@gmail.com>
2023-01-29 10:27:16 -0500

    feat:add Home Link to Nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,7 +5,8 @@ export default function Nav() {
   return (
 <div style={{width: "100%", minHeight: "60px",     backgroundColor:"#d2b48c", paddingLeft: "20px"}}>
           <div className='University_Toolbar'>
-            HOME |
+            <Link href={"../"} style={{color:"black"}}>
+            HOME |</Link>
             </div>
             <div className='University_Toolbar'>ABOUT US | 
               </div>
